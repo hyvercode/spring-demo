f51654f60bb8eb51e53f24c6b4eb17f0d6bdff04
mohirwanh <mohirwanh@gmail.com>
2022-07-04 22:54:48 +0700

    Create Base Response

--- src/main/java/com/hyvercode/demo/helpers/ResponseHandler.java
+++ src/main/java/com/hyvercode/demo/helpers/ResponseHandler.java
@@ -8,13 +8,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseHandler {
-    public static ResponseEntity<Object> baseResponse(HttpStatus status, Object responseObj,int code,String message) {
-        Map<String, Object> map = new HashMap<String, Object>();
+
+    public ResponseHandler() {
+    }
+
+    public static ResponseEntity<Object> baseResponse(HttpStatus httpStatus, Object responseObj, int code, String message) {
+        Map<String, Object> map = new HashMap<>();
         map.put("content", responseObj);
-        map.put("code", code);
+        map.put("status", code);
         map.put("message", message);
         map.put("time_stamp",new Date());
 
-        return new ResponseEntity<Object>(map,status);
+        return new ResponseEntity<>(map,httpStatus);
     }
 }
